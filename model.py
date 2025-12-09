2244fc9992c79f554a5eb59a302e9c59fe29f2ac
dilnipath <dpathirana500@gmail.com>
2025-12-09 11:00:13 -0500

    edited model and ngram

--- model.py
+++ model.py
@@ -7,63 +7,6 @@ from transformers import Trainer, TrainingArguments
 from datasets import Dataset
 from sklearn.metrics import f1_score, accuracy_score, recall_score
 
-df = pd.read_csv("data/combined_drug_instructions.csv")
-
-df = df.rename(columns={"instructions": "sentence", "generated": "labels"})
-
-train_df, test_df = train_test_split(df, test_size=0.3, random_state=42)
-
-val_df, test_df = train_test_split(test_df, test_size=0.5, random_state=0)
-
-train_ds = Dataset.from_pandas(train_df)
-test_ds = Dataset.from_pandas(test_df)
-val_ds = Dataset.from_pandas(val_df)
-
-tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
-
-for index, row in df.iterrows():
-    sentence = row["sentence"];
-    beginning = random.randint(0, len(sentence) - 512)
-    truncated_sentence = sentence[beginning:beginning + 512]
-    df.at[index, "sentence"] = truncated_sentence
-
-def tokenize(batch):
-    return tokenizer(
-        batch["sentence"],
-        truncation=True,
-        padding="max_length",
-        max_length=512,
-    )
-
-train_ds = train_ds.map(tokenize, batched=True)
-test_ds = test_ds.map(tokenize, batched=True)
-val_ds = val_ds.map(tokenize, batched=True)
-
-#Set format for PyTorch
-train_ds.set_format("torch")
-test_ds.set_format("torch")
-val_ds.set_format("torch")
-
-model = RobertaForSequenceClassification.from_pretrained("roberta-base", num_labels=2)
-
-training_args = TrainingArguments(
-    output_dir="./results",
-    num_train_epochs=3,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
-    logging_steps=20,
-    evaluation_strategy="epoch",
-)
-
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=train_ds,
-    eval_dataset=val_ds,
-)
-
-trainer.train()
-
 def arg_max(model_preds):
     label_preds = []
     for i in model_preds:
@@ -78,17 +21,78 @@ def evaluate(type, labels, preds):
     recall = recall_score(labels, preds)
     print(f"{type} RECALL SCORE: ", recall)
 
-train_preds, train_label_ids, train_metrics = trainer.predict(train_ds)
-preds = arg_max(train_preds)
-evaluate("train", train_df["labels"], preds)
+def tokenize(batch):
+    tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
 
-val_preds, val_label_ids, val_metrics = trainer.predict(val_ds)
-preds = arg_max(val_preds)
-evaluate("val", val_df["labels"], preds)
+    return tokenizer(
+        batch["sentence"],
+        truncation=True,
+        padding="max_length",
+        max_length=512,
+    )
 
-test_preds, test_label_ids, test_metrics = trainer.predict(test_ds)
-preds = arg_max(test_preds)
-evaluate("test", test_df["labels"], preds)
+def model(fname):
+    df = pd.read_csv(fname)
 
+    df = df.rename(columns={"instructions": "sentence", "generated": "labels"})
 
+    train_df, test_df = train_test_split(df, test_size=0.3, random_state=42)
+
+    val_df, test_df = train_test_split(test_df, test_size=0.5, random_state=0)
+
+    train_ds = Dataset.from_pandas(train_df)
+    test_ds = Dataset.from_pandas(test_df)
+    val_ds = Dataset.from_pandas(val_df)
+
+    for index, row in df.iterrows():
+        sentence = row["sentence"]
+        beginning = random.randint(0, len(sentence) - 512)
+        truncated_sentence = sentence[beginning:beginning + 512]
+        df.at[index, "sentence"] = truncated_sentence
+
+    train_ds = train_ds.map(tokenize, batched=True)
+    test_ds = test_ds.map(tokenize, batched=True)
+    val_ds = val_ds.map(tokenize, batched=True)
+
+    #Set format for PyTorch
+    train_ds.set_format("torch")
+    test_ds.set_format("torch")
+    val_ds.set_format("torch")
+
+    model = RobertaForSequenceClassification.from_pretrained("roberta-base", num_labels=2)
+
+    training_args = TrainingArguments(
+        output_dir="./results",
+        num_train_epochs=3,
+        per_device_train_batch_size=8,
+        per_device_eval_batch_size=8,
+        logging_steps=20,
+        evaluation_strategy="epoch",
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_ds,
+        eval_dataset=val_ds,
+    )
+
+    trainer.train()
+
+    train_preds, train_label_ids, train_metrics = trainer.predict(train_ds)
+    preds = arg_max(train_preds)
+    evaluate("train", train_df["labels"], preds)
+
+    val_preds, val_label_ids, val_metrics = trainer.predict(val_ds)
+    preds = arg_max(val_preds)
+    evaluate("val", val_df["labels"], preds)
+
+    test_preds, test_label_ids, test_metrics = trainer.predict(test_ds)
+    preds = arg_max(test_preds)
+    evaluate("test", test_df["labels"], preds)
+
+def main():
+    model("data/Human_ChatGPTGen.csv")
+
+main()
 
--- ngram.py
+++ ngram.py
@@ -2,9 +2,6 @@ import random
 import nltk
 from nltk.util import ngrams
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from datasets import Dataset
-from transformers import RobertaTokenizer, RobertaForSequenceClassification
 
 def jaccard_distance(a, b):
     """Calculate the Jaccard distance between sets A and B"""
@@ -12,20 +9,25 @@ def jaccard_distance(a, b):
     b = set(b)
     return 1.0 * len(a & b) / len(a | b)
 
-df = pd.read_csv("data/Human_ChatGPTGen.csv")
+def ngram(fname):
+    df = pd.read_csv("fname")
 
-df = df.rename(columns={"instructions": "sentence", "generated": "labels"})
+    df = df.rename(columns={"instructions": "sentence", "generated": "labels"})
 
-similarity_score = 0
-for i in range(500):
-    sentence1 = df.iloc[i, 1]
-    human_words = nltk.word_tokenize(sentence1)
-    sentence2 = df.iloc[i+500, 1]
-    chat_words = nltk.word_tokenize(sentence2)
-    
-    humangrams = list(ngrams(human_words, 10))
-    chatgrams = list(ngrams(chat_words, 10))
+    similarity_score = 0
+    for i in range(500):
+        sentence1 = df.iloc[i, 1]
+        human_words = nltk.word_tokenize(sentence1)
+        sentence2 = df.iloc[i+500, 1]
+        chat_words = nltk.word_tokenize(sentence2)
+        
+        humangrams = list(ngrams(human_words, 10))
+        chatgrams = list(ngrams(chat_words, 10))
 
-    similarity_score += jaccard_distance(humangrams, chatgrams)
-print("Average Similarity Score: ", similarity_score/500)
+        similarity_score += jaccard_distance(humangrams, chatgrams)
+    print("Average Similarity Score: ", similarity_score/500)
 
+def main():
+    ngram("data/Human_ChatGPTGen.csv")
+
+main()
